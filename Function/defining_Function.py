800d27000e9203b27505a65100ec33ae77d95e70
Hellnight2005 <abhijeet2005shinde.com>
2023-12-26 20:33:15 +0530

    function

---
+++ Function/defining_Function.py
@@ -0,0 +1,21 @@
+# A function is piece of code that performe specified purpose has its own scope and is referred to by name 
+# All function may hav Zero or no or more than one parameter. 
+
+# there are two type of function mainly us 
+# 1) built-in function :- it is created by system the user can not write it it only has read avaible
+# 2) User-define function :- it created by user for reapeted task it can use muliptly
+# "def" Keyworld is use for define function 
+
+def PrintString(string):
+    return print(string)
+
+
+PrintString("Abhijeet")
+
+def AreaOfCircle(r):
+    A= 2*3.14*r*r
+    return A
+
+a=int(input("Enter radius of circle"))
+b=AreaOfCircle(a)
+print(f"Area of circle is {b}")
